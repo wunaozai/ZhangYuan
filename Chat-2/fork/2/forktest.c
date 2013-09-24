ca3e0a6c214dc4e8b94b2de684f99476286163d2
wunaozai <wunaozai@gmail.com>
2013-09-24 15:21:09 +0800

    Update forktest.c

--- Chat-2/fork/2/forktest.c
+++ Chat-2/fork/2/forktest.c
@@ -4,7 +4,7 @@
  * @Created: 2013-08-04 05:34
  * @Author: 林明晓 -- 563804489@qq.com
  * @Description: 
- * @GitHub: https://github.com/wunaozai/Linux_Zhanyuan
+ * @GitHub: https://github.com/wunaozai/Zhanyuan
  *
  * @History: 
  ****************************************/
